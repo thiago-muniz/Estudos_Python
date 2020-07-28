26db6a030e142844c2cfe58aba119806a8c3ea81
Thiago Muniz <thiagotsocial@gmail.com>
2020-07-28 12:54:41 -0300

    Create ex080 Lista ordenada sem repetições.py

---
+++ ex080 Lista ordenada sem repetições.py
@@ -0,0 +1,6 @@
+'''Crie um programa que vai ler vários números e colocar em uma lista.
+Depois disso mostre:
+a) Quantos números foram digitados
+b) A lista ordenada de forma decrescente
+c) Se o valor 5 foi digitado e está ou não na lista
+'''
