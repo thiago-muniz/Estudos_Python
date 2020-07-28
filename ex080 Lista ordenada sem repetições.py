802268360fb565851048ea04f84b2b87db2f3c81
Thiago Muniz <thiagotsocial@gmail.com>
2020-07-28 12:59:55 -0300

    comecei o exercício

--- ex080 Lista ordenada sem repetições.py
+++ ex080 Lista ordenada sem repetições.py
@@ -4,3 +4,8 @@ a) Quantos números foram digitados
 b) A lista ordenada de forma decrescente
 c) Se o valor 5 foi digitado e está ou não na lista
 '''
+lista = []
+while True:
+    lista.append(int(input('Digite um número: ')))
+
+print(lista)
