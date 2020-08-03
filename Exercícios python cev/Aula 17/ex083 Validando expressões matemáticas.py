ece2cb25f130a1bb89ba183f24447efd7060fc9f
Thiago Muniz <thiagotsocial@gmail.com>
2020-08-03 17:33:22 -0300

    83 final
    corrigido com resposta

--- Exercícios python cev/Aula 17/ex083 Validando expressões matemáticas.py
+++ Exercícios python cev/Aula 17/ex083 Validando expressões matemáticas.py
@@ -3,21 +3,18 @@ Crie um programa onde o usuário digite uma expressão qualquer que use parênte
 Seu aplicativo deverá analisar ser a expressão passada está com os parênteses abertos
 e fechados na ordem correta.
 '''
-lista = str(input('Digite uma expressão com parênteses: '))
-lista.split()
-if '('and')' in lista :                             #Primeira avaliação - se tem  '(' e ')' na lista
-    if lista.count('(') == lista.count(')'):        #Segunda avaliação - se tem em pares os elementos
-        if lista.find('(') > lista.find(')'):       #Terceira avalaiação - se a primeira ocorrencia de ')' acontece antes de '(' esta errado
-            print('Sua lista está errada!')
+expressao = str(input('Digite uma expressão com parênteses: '))
+pilha = list()
+for parenteses in expressao:
+    if '(' in expressao:
+        pilha.append('(')
+    elif ')' in expressao:
+        if len(pilha) > 0:
+            pilha.pop()
         else:
-            print('4 análise')                      #Quarta avaliação -
-            for n in range(0, len(lista)):
-                if lista[n] == '(':
-                print(lista[n])
-    else:
-        print('Sua Expressão está errada!')
+            pilha.append(')')
+if len(pilha) == 0:
+    print('sua expressão está certa!')
 else:
-    print('Sua Expressão está errada!')
-
-
+    print('Sua expressão está errada!')
 print(lista)
