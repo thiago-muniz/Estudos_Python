b0ab85e422cda393ef4421c60a12ff27f4da86d9
Thiago Muniz <thiagotsocial@gmail.com>
2020-08-14 17:38:36 -0300

    Update ex091  Jogo de Dados em Python.py

--- Exercícios python cev/Aula 19/ex091  Jogo de Dados em Python.py
+++ Exercícios python cev/Aula 19/ex091  Jogo de Dados em Python.py
@@ -5,58 +5,19 @@ No final, coloque esse dicionário em ordem,
 sabendo que o vencedor tirou o maior número no dado.
 '''
 from random import randint
-
+from operator import itemgetter
 pdicio = dict()
-rank = []
-rank2 = []
-
 print('Valores Sorteados:')
-for c in range(1, 5):
-    pdicio[f'Jogador {c}'] = randint(1, 6)
-    rank.append(pdicio.copy())
-    for k, v in pdicio.items():
-        print(f'O {k} tirou {v} no dado.')
-    del pdicio[f'Jogador {c}']
-
-maior = menor = 0
-print(rank)
-for item in rank:
-    for v in item.values():
-        if len(rank2) == 0:
-            maior = menor = v
-            rank2.append(item)
-
-        elif len(rank2) == 1:
-            if v <= maior:
-                rank2.insert(0, item)
-            elif v < menor:
-                rank2.insert(0, item)
-
-        elif len(rank2) == 2:
-            if v >= maior:
-                maior = v
-                rank2.insert(-1, item)
-            elif v < menor:
-                menor = v
-                rank2.insert(0, item)
-            elif v < maior and v >= menor:
-                rank2.insert(1, item)
-
-        elif len(rank2) == 3:
-            if v >= maior:
-                maior = v
-                rank2.insert(-1, item)
-            elif v < menor:
-                menor = v
-                rank2.insert(0, item)
-            elif v == menor:
-                rank2.insert(1, item)
-            elif v < maior and v > menor:
-                rank2.insert(2, item)
-
+pdicio = {'Jogador 1': randint(1, 6),
+          'jogador 2': randint(1, 6),
+          'Jogador 3': randint(1, 6),
+          'Jogador 4': randint(1, 6)}
+rank = list()
+for k, v in pdicio.items():
+    print(f'O {k} tirou {v} no dado.')
+rank = sorted(pdicio.items(), key=itemgetter(1), reverse=True)
 print('-=' * 25)
 print(f'{"== RANKING DOS JOGADORES == ":^40}')
-for pos, c in enumerate(rank2):
-    for k , v in rank2[pos].items():
-        print(f'{pos+1}º colocado foi {k}, com valor {v}. ')
+for i, v in enumerate(rank):
+    print(f'{i+1}º colocado foi {v[0]} com valor {v[1]}. ')
 
