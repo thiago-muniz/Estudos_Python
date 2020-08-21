ad24d2370b79f042cb07eddb55556612bbc71c1e
Thiago Muniz <thiagotsocial@gmail.com>
2020-08-21 14:51:46 -0300

    Create ex097  Um print especial.py

---
+++ Exercícios python cev/Aula 20/ex097  Um print especial.py
@@ -0,0 +1,21 @@
+'''
+Faça um programa que tenha uma função chamada escreva(),
+que receba um texto qualquer como parâmetro
+e mostre uma mensagem com tamanho adaptável.
+
+Ex:
+escreva('Olá, Mundo!')
+Saída:
+~~~~~~~~~
+ Olá, Mundo!
+~~~~~~~~~
+'''
+def escreva(msg):
+    print('≈' * (len(msg) + 6))
+    print(f'   {msg}')
+    print('≈' * (len(msg) + 6))
+
+#Programa principal
+escreva('Gustavo Guanabara')
+escreva('Cruso de Python no youtube')
+escreva('CeV')
