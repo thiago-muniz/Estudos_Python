a9da9b5b6ee9958bc2211e4ecdab3c669ce76a94
Thiago Muniz <thiagotsocial@gmail.com>
2020-09-08 20:47:42 -0300

    aula módulos 22

---
+++ Exercícios python cev/Aula 22/moeda.py
@@ -0,0 +1,16 @@
+def aumentar(num, perc=10/100):
+    return num + (num * perc)
+
+
+def diminuir(num, perc=10/100):
+    return num - (num * perc)
+
+
+def dobro(num):
+    return num * 2
+
+
+def metade(num):
+    return num / 2
+
+
