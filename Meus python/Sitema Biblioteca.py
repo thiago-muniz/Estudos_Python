9832e1ef4fb64d35ee32fd2c0e57ed672aae0553
Thiago Muniz <thiagotsocial@gmail.com>
2020-09-22 10:31:22 -0300

    remove

--- Meus python/Sitema Biblioteca.py
+++
@@ -1,8 +0,0 @@
-biblioteca = []
-livro = {}
-for c in range(0, 2):
-    livro['título'] = str(input('Título do Livro: '))
-    livro['ano'] = int(input('Ano de lançamento: '))
-    livro['autor'] = str(input('Autor: '))
-    biblioteca.append(livro.copy())
-print(biblioteca)
